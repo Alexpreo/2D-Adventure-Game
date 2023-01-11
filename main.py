b8d069aecf593f4b4c2f420bd0348e1666470a6a
Alexpreo <alexander.preo@gmail.com>
2023-01-11 11:21:00 -0800

    inti

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+size = width, height = 300, 200
+black = 0, 0, 0
+white = 255, 255, 255,
+
+screen = pygame.display.set.mode(size)
+
+pygame.font.init()
+
+
+#running = True
+#while running:
+    
