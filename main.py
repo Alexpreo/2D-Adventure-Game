99b96cb50fea79b776d4387fb769ed42e5f791f8
Alexpreo <alexander.preo@gmail.com>
2023-01-11 13:14:19 -0800

    beginning stage

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from pygame.locals import *
-
 pygame.init()
 
-size = width, height = 300, 200
-black = 0, 0, 0
 white = 255, 255, 255,
+black = 0, 0, 0
+size = width, height = 600, 600
+screen = pygame.display.set_mode(size)
+icon = pygame.image.load()
 
-screen = pygame.display.set.mode(size)
 
-pygame.font.init()
-
-
-#running = True
-#while running:
+running = True
+while running:  
     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill(white)
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 50)
+    pygame.display.set_caption("2D Adventure Game")
+    pygame.display.set_icon(icon)
+    pygame.display.flip()
+
+pygame.quit()
